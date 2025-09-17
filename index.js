f3a27796e8af0b210785b8c85c2b933eabb83301
Fabricio Salustiano <fabricio.silva@fieldcontrol.com.br>
2025-09-17 17:28:58 -0300

    fix: remove commented test line and adjust log placement in server initialization

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 
-// Teste de alteração
-
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello CI/CD World");
 });
+
+console.log("Iniciando o servidor...");
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);

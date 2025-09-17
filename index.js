a86d64ff9ac37a3d60eb2ae93626421555c765ed
Fabricio Salustiano <fabricio.silva@fieldcontrol.com.br>
2025-09-16 22:49:28 -0300

    refactor: remove server initialization log from index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-console.log("Iniciando o servidor...");
-
 app.get("/", (req, res) => {
   res.send("Hello CI/CD World");
 });

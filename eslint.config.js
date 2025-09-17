7fb22638a758e7fe6f41eadd8b3387e6eaabeedd
Fabricio Salustiano <fabricio.silva@fieldcontrol.com.br>
2025-09-16 22:11:55 -0300

    feat: configurar projeto Node com Express e ESLint (ESM)
    - adicionar servidor Express básico com type=module
    - configurar ESLint usando flat config (eslint.config.js)
    - adicionar dependência globals para suporte ao Node/ES2021
    - criar scripts npm: lint e lint:fix

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"]
+    }
+  }
+];
